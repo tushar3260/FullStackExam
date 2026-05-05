61fd125600dd29511276b077949cd7fdbfe24b52
Tushararya <tushararya773@gmail.com>
2026-05-05 15:37:14 +0530

    notice  model updated

--- src/main/java/org/example/fullstackexam/model/Notice.java
+++ src/main/java/org/example/fullstackexam/model/Notice.java
@@ -1,23 +1,33 @@
 package org.example.fullstackexam.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.time.LocalDateTime;
 
+@Document(collection = "notices")
 public class Notice {
 
+    @Id
     private String id;
     private String title;
     private String message;
-    private LocalDateTime createdAt;
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public Notice() {}
+    public Notice(String id, String title, String message, LocalDateTime createdAt) {
+        this.id = id;
+        this.title = title;
+        this.message = message;
+        this.createdAt = createdAt;
+    }
 
     public String getId() { return id; }
     public void setId(String id) { this.id = id; }
-
     public String getTitle() { return title; }
     public void setTitle(String title) { this.title = title; }
-
     public String getMessage() { return message; }
     public void setMessage(String message) { this.message = message; }
-
     public LocalDateTime getCreatedAt() { return createdAt; }
     public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
 }
