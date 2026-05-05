8f0cbd4c2a41c0636ededd9bdc287e7f25522638
tushar3260 <tusharbansal3260@gmail.com>
2026-05-05 13:37:34 +0530

    Initial Setup

---
+++ src/main/java/org/example/fullstackexam/FullStackExamApplication.java
@@ -0,0 +1,13 @@
+package org.example.fullstackexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullStackExamApplication.class, args);
+    }
+
+}
