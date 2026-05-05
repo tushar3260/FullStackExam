4b96eac753bf67ddf659c0b64b0536bc4007a964
Tushararya <tushararya773@gmail.com>
2026-05-05 15:06:10 +0530

    addTenantservices

--- src/main/java/org/example/fullstackexam/repository/TenantRepository.java
+++ src/main/java/org/example/fullstackexam/repository/TenantRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.example.fullstackexam.model.Tenant;
 
 public interface TenantRepository extends MongoRepository<Tenant, String> {
+
 }
