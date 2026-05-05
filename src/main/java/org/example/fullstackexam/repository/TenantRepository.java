9caaacb22f54f460809e0a9391c7fc1c2c1d8016
Shagun Nayak <shagun.nayak_cs23@gla.ac.in>
2026-05-05 14:42:10 +0530

    TenantRep

---
+++ src/main/java/org/example/fullstackexam/repository/TenantRepository.java
@@ -0,0 +1,7 @@
+package org.example.fullstackexam.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.example.fullstackexam.model.Tenant;
+
+public interface TenantRepository extends MongoRepository<Tenant, String> {
+}
